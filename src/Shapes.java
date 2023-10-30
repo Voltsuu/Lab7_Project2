1f837ea5c519323dbee0247c250a3186b926e0c0
Voltsuuu <npoudel2007@gmail.com>
2023-10-30 09:39:54 -0400

    done

---
+++ src/Shapes.java
@@ -0,0 +1,28 @@
+public class Shapes {
+    public static void main(String[] args) {
+        String symbol = "*";
+        //Regular triangle
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("");
+        //Inverted triangle
+        for (int i = 5; i >= 1; i--) {
+            for (int j = i; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("");
+        //Block.
+        for (int i = 1; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
